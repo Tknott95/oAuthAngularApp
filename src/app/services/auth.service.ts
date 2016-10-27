9bfcd1402e4b913579e426d3835d057abca9f86a
tknott95 <tknott95@hotmail.com>
2016-10-26 21:17:17 -0600

    removed service from git for security

--- src/app/services/auth.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { tokenNotExpired } from 'angular2-jwt';
-
-declare var Auth0Lock: any;
-
-@Injectable()
-export class Auth {
-    //configure Auth0
-}
