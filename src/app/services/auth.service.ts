02a2f57767e688492578fcbda627cbbbfcb95496
tknott95 <tknott95@hotmail.com>
2016-10-26 21:11:43 -0600

    Authorizing Auth0 .  Adding gitignore services

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { tokenNotExpired } from 'angular2-jwt';
+
+declare var Auth0Lock: any;
+
+@Injectable()
+export class Auth {
+    //configure Auth0
+}
